2d25ecfcb6f6dc2b1ea14a56359b2d97a29b573e
alperarslannn <alper.arslan663@gmail.com>
2024-05-10 13:09:49 +0300

    role and authority ambiguity is fixed, endpoint authorization is implemented

--- Account Service (Java)/task/src/account/exception/RoleNotFoundException.java
+++ Account Service (Java)/task/src/account/exception/RoleNotFoundException.java
@@ -5,13 +5,12 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 
 import java.time.LocalDateTime;
 
-@ResponseStatus(code = HttpStatus.NOT_FOUND, reason = "User not found!")
+@ResponseStatus(code = HttpStatus.NOT_FOUND, reason = "Role not found!")
 public class RoleNotFoundException extends BaseException {
-    private final LocalDateTime timestamp;
+    private final LocalDateTime timestamp = LocalDateTime.now();
 
     public RoleNotFoundException() {
         super();
-        timestamp = LocalDateTime.now();
     }
 
     public LocalDateTime getTimestamp() {
